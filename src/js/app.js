17ff27c4cd4c104e7cfec38998ec0ebee1d6a0aa
Martin del Pino <dellpinos7@gmail.com>
2023-03-01 20:31:41 -0300

    termimando wordFlip

--- src/js/app.js
+++ src/js/app.js
@@ -2,35 +2,33 @@ function main() {
     wordFlip();
 }
 
-
 function wordFlip() {
-
-
+    const botonFormulario = document.querySelector('#botonFormulario');
     const formulario = document.querySelector('.formularioWF');
     const texto = document.querySelector('#inputWordFlip');
     const resultado = document.querySelector('#resultadoWF');
 
+    botonFormulario.addEventListener('click', function(){
+        const stringIngresado = texto.value;
+        invertirPalabra(stringIngresado);
+    });
 
     formulario.addEventListener('submit', function(e) {
         e.preventDefault();
         const stringIngresado = texto.value;
         invertirPalabra(stringIngresado);
-        
     });
     
-
     function invertirPalabra(string) {
 
-        console.log(string);
+//        console.log(string);
 
         let array = string.split(' '); //separa por palabras
-
-
         let acu = 0;
         while (acu < array.length) {
             let randomNumber = Math.floor(Math.random() * 100); //random number
             if (randomNumber % 2 === 0) { // palabra elegida
-                console.log(array[acu]);
+//                console.log(array[acu]);
                 let arrayDos = array[acu].split('');  // contiene cada letra de la palabra
                 let acu2 = 0;
                 let valorUno = [];
@@ -44,14 +42,9 @@ function wordFlip() {
             acu++;
 
         }
-
-        console.log(array.join(' '));
-        resultado.textContent = array.join(' ');
+//        console.log(array.join(' '));
         resultado.textContent = array.join(' ');
     }
-
-
-
 }
 
 main();
